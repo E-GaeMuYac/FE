25a333f05fdb8ddfaa6cce9fac9ea594b12d068c
soojung01 <ksj29622@naver.com>
2023-01-10 01:59:52 +0900

    FEAT: [추가] 네이버로그인 버튼이벤트 및 인가코드 전달 추가 구현

--- src/components/socialLogin/NaverRedirect.jsx
+++ src/components/socialLogin/NaverRedirect.jsx
@@ -2,6 +2,22 @@ import axios from 'axios';
 import React, { useEffect } from 'react';
 
 const NaverRedirect = () => {
+  const code = new URL(window.location.href).search.split('&')[0].split('=')[1];
+
+  const api = axios.create({ baseURL: process.env.REACT_APP_API_ENDPOINT });
+
+  useEffect(() => {
+    NaverLogin(code);
+  }, []);
+
+  const NaverLogin = async (code) => {
+    try {
+      const res = await api.get(`/api/users/login/naver?code=${code}`);
+      console.log(res);
+    } catch (e) {
+      alert(e);
+    }
+  };
   return;
 };
 
