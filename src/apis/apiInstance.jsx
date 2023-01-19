0d1c9c8fe9ddc133b972ae0598261b6f88be3b46
soojung01 <ksj29622@naver.com>
2023-01-19 20:34:30 +0900

    FEAT: [추가] refreshToken 만료 시 에러처리 인터셉터 추가

--- src/apis/apiInstance.jsx
+++ src/apis/apiInstance.jsx
@@ -4,6 +4,25 @@ export const api = axios.create({
   baseURL: process.env.REACT_APP_API_ENDPOINT,
 });
 
+api.interceptors.response.use(
+  function (config) {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (!accessToken) {
+      const newAccessToken = config.headers.accesstoken;
+      localStorage.setItem('accessToken', newAccessToken);
+    }
+
+    return config;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.clear();
+      window.location.replace('/login');
+    }
+  }
+);
+
 export const userApi = axios.create({
   baseURL: process.env.REACT_APP_API_ENDPOINT,
 });
@@ -18,13 +37,21 @@ userApi.interceptors.request.use(function (config) {
   return config;
 });
 
-userApi.interceptors.response.use(function (config) {
-  const accessToken = localStorage.getItem('accessToken');
+userApi.interceptors.response.use(
+  function (config) {
+    const accessToken = localStorage.getItem('accessToken');
 
-  if (!accessToken) {
-    const newAccessToken = config.headers.accesstoken;
-    localStorage.setItem('accessToken', newAccessToken);
+    if (!accessToken) {
+      const newAccessToken = config.headers.accesstoken;
+      localStorage.setItem('accessToken', newAccessToken);
+    }
+
+    return config;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.clear();
+      window.location.replace('/login');
+    }
   }
-
-  return config;
-});
+);
