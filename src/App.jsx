70667c9259b3b3fe307db101ec84784d54b35355
soojung01 <ksj29622@naver.com>
2023-01-21 13:22:41 +0900

    STYLE: [개선] 회원탈퇴 모달 디자인 변경

--- src/App.jsx
+++ src/App.jsx
@@ -90,14 +90,14 @@ function App() {
         <Route
           path='/mypage'
           element={
-            <Layout>
+            <>
               <User
                 setuserimage={setUserImage}
                 setistoken={setIsToken}
                 istoken={isToken}
               />
               <CompareBox />
-            </Layout>
+            </>
           }
         />
         {/* <Route path='/spinner' element={<Spinner />} /> */}
