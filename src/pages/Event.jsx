9205347f7043cdfb2bfe233d38c71a29fbf8e193
soojung01 <ksj29622@naver.com>
2023-02-01 02:11:43 +0900

    FEAT: [개선] 푸터에서 URL 이동 시 스크롤 상단으로 이동

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 const Event = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <Wrap>
       <EventImg />
