620a97f0ec8bc4d758de67a0505bedf427cf95bc
이재정 <jj9535@naver.com>
2023-02-02 15:22:11 +0900

    FIX: [수정] 알러지 성분에 따라 그래프 색이 바뀌도록 수정

--- src/pages/Compare.jsx
+++ src/pages/Compare.jsx
@@ -180,6 +180,12 @@ const ComparePage = () => {
           medicineA: 0,
           medicineB: 0,
         };
+        if (versusList[0].materialName[i].allergy) {
+          newMedicineData['columnSettings'] = {
+            fill: am5.color(0xc4ad83),
+            stoke: am5.color(0xff392b),
+          };
+        }
         graphData.push(newMedicineData);
       }
       //두 번째 약
@@ -190,6 +196,12 @@ const ComparePage = () => {
           medicineA: 0,
           medicineB: 0,
         };
+        if (versusList[1].materialName[i].allergy) {
+          newMedicineData['columnSettings'] = {
+            fill: am5.color(0xff392b),
+            stoke: am5.color(0xff392b),
+          };
+        }
         graphData.push(newMedicineData);
       }
       //중복 제거
@@ -280,119 +292,20 @@ const ComparePage = () => {
           series
             .get('colors')
             .set('colors', [
-              versusList[0].materialName[0]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x091a7a),
-              versusList[0].materialName[1]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x102693),
-              versusList[0].materialName[2]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x1939b7),
-              versusList[0].materialName[3]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x254edb),
-              versusList[0].materialName[4]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x3366ff),
-              versusList[0].materialName[5]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x6690ff),
-              versusList[0].materialName[6]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x84a9ff),
-              versusList[0].materialName[7]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0xadc8ff),
-              versusList[0].materialName[8]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x13097a),
-              versusList[0].materialName[9]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x1d1093),
-              versusList[0].materialName[10]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x2b19b7),
-              versusList[0].materialName[11]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x3c25db),
-              versusList[0].materialName[12]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x5033ff),
-              versusList[0].materialName[13]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x7e66ff),
-              versusList[0].materialName[14]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x9984ff),
-              versusList[0].materialName[15]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0xbcadff),
-              versusList[0].materialName[16]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x0b2d78),
-              versusList[0].materialName[17]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x124092),
-              versusList[0].materialName[18]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x1d5cb5),
-              versusList[0].materialName[19]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x2a7bd8),
-              versusList[0].materialName[20]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x3a9efc),
-              versusList[0].materialName[21]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x6bbdfd),
-              versusList[0].materialName[22]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x88d1fe),
-              versusList[0].materialName[23]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0xb0e4fe),
+              am5.color(0x091a7a),
+              am5.color(0x102693),
+              am5.color(0x1939b7),
+              am5.color(0x254edb),
+              am5.color(0x3366ff),
+              am5.color(0x6690ff),
+              am5.color(0x84a9ff),
+              am5.color(0xadc8ff),
+              am5.color(0xd6e4ff),
             ]);
-          // series
-          //   .get('colors')
-          //   .set(
-          //     'colors',
-          //     versusList[0].materialName[i].allergy
-          //       ? [
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //           am5.color(0xff392b),
-          //         ]
-          //       : [
-          //           am5.color(0x091a7a),
-          //           am5.color(0x102693),
-          //           am5.color(0x1939b7),
-          //           am5.color(0x254edb),
-          //           am5.color(0x3366ff),
-          //           am5.color(0x6690ff),
-          //           am5.color(0x84a9ff),
-          //           am5.color(0xadc8ff),
-          //           am5.color(0xd6e4ff),
-          //         ]
-          //   );
+
+          series.slices.template.setAll({
+            templateField: 'columnSettings',
+          });
 
           series.data.setAll(graphData);
 
@@ -427,82 +340,82 @@ const ComparePage = () => {
           );
 
           //색상 지정
-          series2
-            .get('colors')
-            .set('colors', [
-              versusList[1].materialName[0]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x091a7a),
-              versusList[1].materialName[1]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x102693),
-              versusList[1].materialName[2]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x1939b7),
-              versusList[1].materialName[3]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x254edb),
-              versusList[1].materialName[4]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x3366ff),
-              versusList[1].materialName[5]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x6690ff),
-              versusList[1].materialName[6]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x84a9ff),
-              versusList[1].materialName[7]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0xadc8ff),
-              versusList[1].materialName[8]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x13097a),
-              versusList[1].materialName[9]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x1d1093),
-              versusList[1].materialName[10]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x2b19b7),
-              versusList[1].materialName[11]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x3c25db),
-              versusList[1].materialName[12]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x5033ff),
-              versusList[1].materialName[13]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x7e66ff),
-              versusList[1].materialName[14]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x9984ff),
-              versusList[1].materialName[15]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0xbcadff),
-              versusList[1].materialName[16]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x0b2d78),
-              versusList[1].materialName[17]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x124092),
-              versusList[1].materialName[18]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x1d5cb5),
-              versusList[1].materialName[19]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x2a7bd8),
-              versusList[1].materialName[20]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x3a9efc),
-              versusList[1].materialName[21]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x6bbdfd),
-              versusList[1].materialName[22]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0x88d1fe),
-              versusList[1].materialName[23]?.allergy
-                ? am5.color(0xff392b)
-                : am5.color(0xb0e4fe),
-            ]);
+          // series2
+          //   .get('colors')
+          //   .set('colors', [
+          //     versusList[1].materialName[0]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x091a7a),
+          //     versusList[1].materialName[1]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x102693),
+          //     versusList[1].materialName[2]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x1939b7),
+          //     versusList[1].materialName[3]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x254edb),
+          //     versusList[1].materialName[4]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x3366ff),
+          //     versusList[1].materialName[5]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x6690ff),
+          //     versusList[1].materialName[6]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x84a9ff),
+          //     versusList[1].materialName[7]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0xadc8ff),
+          //     versusList[1].materialName[8]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x13097a),
+          //     versusList[1].materialName[9]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x1d1093),
+          //     versusList[1].materialName[10]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x2b19b7),
+          //     versusList[1].materialName[11]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x3c25db),
+          //     versusList[1].materialName[12]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x5033ff),
+          //     versusList[1].materialName[13]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x7e66ff),
+          //     versusList[1].materialName[14]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x9984ff),
+          //     versusList[1].materialName[15]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0xbcadff),
+          //     versusList[1].materialName[16]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x0b2d78),
+          //     versusList[1].materialName[17]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x124092),
+          //     versusList[1].materialName[18]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x1d5cb5),
+          //     versusList[1].materialName[19]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x2a7bd8),
+          //     versusList[1].materialName[20]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x3a9efc),
+          //     versusList[1].materialName[21]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x6bbdfd),
+          //     versusList[1].materialName[22]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0x88d1fe),
+          //     versusList[1].materialName[23]?.allergy
+          //       ? am5.color(0xff392b)
+          //       : am5.color(0xb0e4fe),
+          //   ]);
           // series2
           //   .get('colors')
           //   .set(
@@ -544,6 +457,9 @@ const ComparePage = () => {
           //         ]
           //   );
 
+          series2.slices.template.setAll({
+            templateField: 'columnSettings',
+          });
           series2.data.setAll(graphData);
 
           //ticks, 라벨 제거
